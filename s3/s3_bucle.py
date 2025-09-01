b42aa0886a6c8266a61d461f42172c58f5f1ea30
LSGT05 <luissebastianguillen@outlook.com>
2025-09-01 17:24:47 -0500

    bucle modificado

--- s3/s3_bucle.py
+++ s3/s3_bucle.py
@@ -1,7 +1,7 @@
 import random as rd
 voltajes = []
-for i in range(10):
-    num = rd.randint(0, 10)   
+for i in range(100):
+    num = rd.randint(0, 100)   
     voltajes.append(num)
 print("Lista de voltajes:")
 print([f"{v:.2f}" for v in voltajes])  
