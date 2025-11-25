f2aee6cf864043828561f380d1c2f319b25888aa
LSGT05 <luissebastianguillen@outlook.com>
2025-11-24 19:14:42 -0500

    v3.1.1

--- Proyecto_final/src/pipeline/__init__.py
+++ Proyecto_final/src/pipeline/__init__.py
@@ -1,13 +1,15 @@
-from .IO_Utils import Root, ensure_dirs, list_raw_csvs, make_clean_name, safe_stem
-from .processing import procesar_archivo
-from .reporting import generate_report
+from .IO_Utils import (
+    Root,
+    ensure_dirs,
+    list_raw_csvs,
+    make_clean_name,
+    safe_stem,
+)
 
-__all__ = [
-    "Root",
-    "ensure_dirs",
-    "list_raw_csvs",
-    "make_clean_name",
-    "safe_stem",
-    "procesar_archivo",
-    "generate_report",
-]
+from .cleaning import process_file_separating_hysteresis
+from .kpis import compute_kpis
+from .plotting import (
+    plot_voltage_line,
+    plot_voltage_hist,
+    plot_voltage_boxplot,
+)
